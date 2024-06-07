00ba03732f46f189ef2ae43e4988c574b870d2b7
Bruno Fernandez <brunonce07@gmail.com>
2024-06-07 13:27:43 -0300

    ruta formulario persona routing

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
 // config.php
 
-define('URL_PREFIX', '/mvccomposer/');
+define('URL_PREFIX', '/prj/mvc/');
 define('URL_BASE_PATH', 'http://' . $_SERVER['HTTP_HOST'] . URL_PREFIX);
 define('URL_BASE', URL_BASE_PATH . 'index.php?r=');
 define('DB_HOST', 'localhost');
-define('DB_PORT', '3306');  // Puerto estándar de MySQL, cambia según necesidades
+define('DB_PORT', '3366');  // Puerto estándar de MySQL, cambia según necesidades
 define('DB_NAME', 'automotora');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
--- config/routing.php
+++ config/routing.php
@@ -15,3 +15,5 @@ $router->add('autos/listar', ['controller' => 'Auto', 'action' => 'listar']);
 $router->add('autos/editar', ['controller' => 'Auto', 'action' => 'editar']);
 $router->add('autos/modificar', ['controller' => 'Auto', 'action' => 'modificar']);
 $router->add('autos/borrar', ['controller' => 'Auto', 'action' => 'borrar']);
+//ruta al controlador
+$router->add('personas/nuevo', ['controller' => 'Auto', 'action' => 'nuevo']);
